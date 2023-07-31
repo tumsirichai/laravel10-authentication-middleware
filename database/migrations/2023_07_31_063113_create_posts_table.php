4005a0d6e3e716a0dacfceb8b23eddc2815159bf
Sirichai Teerapattarasakul <tump_si@yahoo.com>
2023-07-31 15:07:21 +0700

    post: migration create table and controller

---
+++ database/migrations/2023_07_31_063113_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->index();
+            $table->string('slug')->index();
+            $table->longtext('detail')->nullable();
+            $table->string('image')->nullable();
+            $table->string('status',20)->default('active');
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('category_id')->unsigned()->foreign('category_id')->on('post_categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
