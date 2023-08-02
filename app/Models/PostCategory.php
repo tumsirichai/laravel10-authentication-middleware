efd81ad740a31585e5744ecad7c7ec6fb8b6aefb
Sirichai Teerapattarasakul <tump_si@yahoo.com>
2023-08-02 15:21:59 +0700

    post(api): relation to cate again

--- app/Models/PostCategory.php
+++ app/Models/PostCategory.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\hasMany;
 use Laravel\Sanctum\HasApiTokens;
 
 class PostCategory extends Model
 {
     use HasFactory, HasApiTokens;
+    protected $fillable = ['id','name', 'user_id'];
+
+    public function post():hasMany
+    {
+        return $this->hasMany(Post::class,'category_id');
+    }
 }
